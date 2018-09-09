00fa33c407fb99e6ef2f67a24f20fc8565bf8bf8
beeperdeeper089 <vyom.fadia.work@gmail.com>
2018-09-09 17:18:38 +0100

    Add client folder and basic CMake & Folder structure

---
+++ client/source/include/client.hpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+class Client
+{
+public:
+	void fillerFunction() { std::cout << "Hello Client!" << std::endl; }
+};
---
+++ client/source/src/main.cpp
@@ -0,0 +1,10 @@
+#include "client.hpp"
+
+int main()
+{
+	Client* helloworld;
+
+	helloworld = new Client();
+
+	helloworld->fillerFunction();
+}
