2dee5a83f3b028d330b5621834545a37cb6f71e8
apachano <pachano.austin@gmail.com>
2018-10-07 12:00:21 -0700

    Add Block Getters and Setters

--- engine/src/voxels/block.cpp
+++ engine/src/voxels/block.cpp
@@ -8,7 +8,7 @@
 
 using namespace pheonix;
 
-void Block:init(std::string id, std::string name, std::string description,
+void Block::init(std::string id, std::string name, std::string description,
 int rotation, BlockType type){
 
     this.id = id;
@@ -17,4 +17,23 @@ int rotation, BlockType type){
     this.rotation = rotation;
     this.type = type;
 
+};
+
+std::string Block::getID(){
+    return this.id;
+};
+std::string Block::getName(){
+    return this.name;
+};
+std::string Block::getDescription(){
+    return this.description;
+};
+BlockType Block::getBlockType(){
+    return this.type;
+};
+int Block::getRotation(){
+    return this.rotation
+};
+void setRotation(int rotation){
+    this.rotation = rotation;
 };
