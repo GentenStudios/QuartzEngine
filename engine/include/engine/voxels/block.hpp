6d8ee11f2b38374131c146b64a0400661b27c3a7
apachano <pachano.austin@gmail.com>
2018-10-07 12:24:16 -0700

    Add Doxygen Comments

--- engine/include/engine/voxels/block.hpp
+++ engine/include/engine/voxels/block.hpp
@@ -10,6 +10,8 @@ namespace pheonix {
 
     namespace voxels{
 
+        /** @brief This defines what state of matter the block is
+         */
         enum class BlockType
         {
             GAS,
@@ -20,26 +22,51 @@ namespace pheonix {
         class Block{
 
         private:
-            std::string id; //Unique id using the convention mod:name
-            std::string name; //Name that will display to the user
-            std::string description; //Short description of the block
-            int rotation; //Direction block is facing
-            BlockType blockType; //What type of block are we dealing with?
+            /// @breif Unique id using the convention mod:name
+            std::string id;
+            /// @breif Name that will display to the user
+            std::string name;
+            /// @breif Short description of the block
+            std::string description;
+            /// @breif Direction block is facing
+            int rotation;
+            /// @breif What type of block are we dealing with?
+            BlockType blockType;
             //??????? textures; //String? Array of strings? Array of file pointers? Image class?
 
         public:
-            Block(std::string id, std::string name, std::string description,
-                  int rotation, BlockType type);
+            /**
+             * @brief Initialise a block
+             * @param[in] The unique id for the block, should be in format mod:id to eliminate conflict
+             * @param[in] The player friendly name of the block
+             * @param[in] A short description of what the block is
+             * @param[in] The state of block rotation
+             * @param[in] The block type (SOLID, LIQUID, GAS)
+             */
+            static void init(std::string id, std::string name, std::string description, int rotation, BlockType type);
 
+            /// @breif[in] Getter, returns the unique id of the block
             std::string getID();
+
+            /// @breif[in] Getter, returns the player friendly name of the block
             std::string getName();
+
+            /// @breif[in] Getter, returns the breif block description
             std::string getDescription();
+
+            /// @breif[in] Getter, Returns the block type (Solid, Liquid, Gas)
             BlockType getBlockType();
+
+            /// @breif[in] Getter, Returns the rotation value of the block
             int getRotation();
+            /// @breif[in] Setter, Sets the rotation value of the block
             void setRotation(int rotation);
+
             //??????? getTextures();
         };
 
+
+        /// @breif[in] Getter, Returns a block object from its unique id
         Block getBlockByID(std::string id);
     }
 
--- engine/src/voxels/block.cpp
+++ engine/src/voxels/block.cpp
@@ -8,9 +8,7 @@
 
 using namespace pheonix;
 
-void Block::init(std::string id, std::string name, std::string description,
-int rotation, BlockType type){
-
+void Block::init(std::string id, std::string name, std::string description, int rotation, BlockType type){
     this.id = id;
     this.name = name;
     this.description = description;
@@ -22,18 +20,26 @@ int rotation, BlockType type){
 std::string Block::getID(){
     return this.id;
 };
+
 std::string Block::getName(){
     return this.name;
 };
+
 std::string Block::getDescription(){
     return this.description;
 };
+
 BlockType Block::getBlockType(){
     return this.type;
 };
+
 int Block::getRotation(){
     return this.rotation
 };
 void setRotation(int rotation){
     this.rotation = rotation;
+};
+
+Block getBlockByID{
+    return NULL;
 };
