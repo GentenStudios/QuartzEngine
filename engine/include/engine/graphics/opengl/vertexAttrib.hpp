767387e9383e2f1f2c56366d76968e437bc40306
Vyom Fadia <vyom.fadia.work@gmail.com>
2018-10-12 17:52:00 +0100

    Write some vertex attribute stuff

---
+++ engine/include/engine/graphics/opengl/vertexAttrib.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "engine/common.hpp"
+#include "engine/graphics/opengl/glTypes.hpp"
+#include <GL/glew.h>
+
+namespace pheonix {
+    namespace graphics {
+        namespace opengl {
+
+            struct VertexAttrib
+            {
+                unsigned int location; ///< Location of attrib, used in shader.
+
+                int components; ///< Number of componenets in the attribute
+
+                GLType type; ///< Attribute Data Type
+
+                VertexAttrib( unsigned int attribLocation, int attribNumComp, GLType attribType) : location( attribLocation ), components( attribNumComp ), type( attribType )
+                {
+                }
+
+                VertexAttrib( unsigned int attribLocation, int attribNumComp ) : VertexAttrib( attribLocation, attribNumComp, GLType::FLOAT )
+                {
+                }
+            };
+
+        }
+    }
+}
