5409e1f37799ef6f2140f99abe56c75afa4c4211
Barney Wilks <barneywilks1@gmail.com>
2018-10-13 19:09:50 +0100

    Implemented more Matrix4x4 functionality

--- engine/include/engine/math/matrix4x4.hpp
+++ engine/include/engine/math/matrix4x4.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "engine/math/vector3.hpp"
+
 namespace phoenix {
 	/**
 	 * @brief Represents a column major 4x4 matrix of floats.
@@ -12,5 +14,39 @@ namespace phoenix {
 		 * @brief Default constructs the as an identity matrix (the top left to bottom right diagonal is 1.0f)
 		 */
 		Matrix4x4();
+
+		/**
+		 * @brief Constructs a perspective projection matrix.
+		 * @param aspect The aspect ratio is the ratio of x (width) to y (height).
+		 * @param fov Specifies the field of view angle, in degrees.
+		 * @param far Distance from the viewer to the far clipping plane (should always be positive)
+		 * @param near Distance from the viewer to the near clipping plane.
+		 * @return The calculated perspective projection matrix.
+		 */
+		static Matrix4x4 perspective(float aspect, float fov, float far, float near);
 	};
+
+    /**
+     * @brief Multiplys two matrices together. Multiplication is not commutative (therefore left*right != right*left)
+     * @param left Left hand side of matrix multiplication.
+     * @param right Right hand side of matrix multiplication.
+     * @return Product of `left` and `right` matrices.
+     */
+	Matrix4x4 operator*(const Matrix4x4& left, const Matrix4x4& right);
+
+    /**
+     * @brief Multiplys together a matrix and a scalar value.
+     * @param left Left hand side of multiplication.
+     * @param right Right hand side of multiplication
+     * @return Product of `left` and `right`
+     */
+	Matrix4x4 operator*(const Matrix4x4& left, const float& right);
+
+    /**
+     * @brief Multipys together a matrix and a Vector3 value.
+     * @param left Left hand side of multiplication.
+     * @param right Right hand side of multiplication.
+     * @return Product of `left` and `right`
+     */
+	Vector3 operator*(const Matrix4x4& left, const Vector3& right);
 }
--- engine/src/math/matrix4x4.cpp
+++ engine/src/math/matrix4x4.cpp
@@ -1,6 +1,9 @@
 #include "engine/math/matrix4x4.hpp"
 
+#include "engine/math/mathutils.hpp"
+
 #include <cstring>
+#include <cmath>
 
 using namespace phoenix;
 
@@ -14,4 +17,60 @@ Matrix4x4::Matrix4x4()
 	elements[INDEX2D(1, 1)] = 1.f;
 	elements[INDEX2D(2, 2)] = 1.f;
 	elements[INDEX2D(3, 3)] = 1.f;
+}
+
+Matrix4x4 Matrix4x4::perspective(float aspect, float fov, float far, float near)
+{
+	Matrix4x4 mat4;
+
+	float t = -(2.f * far * near) / (far - near);
+	float tanHalfFovy = std::tanf(MathUtils::toRadians(fov * 0.5f));
+
+	mat4.elements[0] = 1.f / (aspect * tanHalfFovy);
+	mat4.elements[INDEX2D(1, 1)] = 1.f / tanHalfFovy;
+	mat4.elements[INDEX2D(2, 2)] = -(far + near) / (far - near);
+	mat4.elements[INDEX2D(3, 2)] = -1.f;
+	mat4.elements[INDEX2D(2, 3)] = -(2.f * far * near) / (far - near);
+	mat4.elements[INDEX2D(3, 3)] = 0.f;
+
+	return mat4;
+}
+
+Matrix4x4 phoenix::operator*(const Matrix4x4& left, const Matrix4x4& right)
+{
+	Matrix4x4 matrix;
+
+	for (int x = 0; x < 4; x++)
+	{
+		for (int y = 0; y < 4; y++)
+		{
+			float xy = 0.f;
+			for (int k = 0; k < 4; ++k)
+			{
+				xy += left.elements[INDEX2D(x, k)] * right.elements[INDEX2D(k, y)];
+			}
+			matrix.elements[INDEX2D(x, y)] = xy;
+		}
+	}
+
+	return matrix;
+}
+
+Matrix4x4 phoenix::operator*(const Matrix4x4& left, const float& right)
+{
+	Matrix4x4 matrix;
+	for (int i = 0; i < 16; i++)
+	{
+		matrix.elements[i] = left.elements[i] * right;
+	}
+	return matrix;
+}
+
+Vector3 phoenix::operator*(const Matrix4x4& left, const Vector3& right)
+{
+	float x = left.elements[0 + 0 * 4] * right.x + left.elements[1 + 0 * 4] * right.y + left.elements[2 + 0 * 4] * right.z;
+	float y = left.elements[0 + 1 * 4] * right.x + left.elements[1 + 1 * 4] * right.y + left.elements[2 + 1 * 4] * right.z;
+	float z = left.elements[0 + 2 * 4] * right.x + left.elements[1 + 2 * 4] * right.y + left.elements[2 + 2 * 4] * right.z;
+
+	return { x, y, z };
 }
