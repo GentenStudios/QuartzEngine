160b035184c86c34a0d11aa44b8077c4bc714834
Barney Wilks <barneywilks1@gmail.com>
2018-10-13 15:13:42 +0100

    Inital implementation of Matrix4x4

--- engine/include/engine/math/matrix4x4.hpp
+++ engine/include/engine/math/matrix4x4.hpp
@@ -1,1 +1,16 @@
-//tmp
+#pragma once
+
+namespace phoenix {
+	/**
+	 * @brief Represents a column major 4x4 matrix of floats.
+	 */
+	struct Matrix4x4
+	{
+		float elements[4 * 4]; ///< The 16 float array that stores the matrices elements sequentially.
+
+		/**
+		 * @brief Default constructs the as an identity matrix (the top left to bottom right diagonal is 1.0f)
+		 */
+		Matrix4x4();
+	};
+}
--- engine/src/math/matrix4x4.cpp
+++ engine/src/math/matrix4x4.cpp
@@ -1,1 +1,17 @@
-//tmp
+#include "engine/math/matrix4x4.hpp"
+
+#include <cstring>
+
+using namespace phoenix;
+
+#define INDEX2D(x, y) (x + y * 4)
+
+Matrix4x4::Matrix4x4()
+{
+	for (int i = 0; i < 4 * 4; ++i) elements[i] = 0.f;
+
+	elements[INDEX2D(0, 0)] = 1.f;
+	elements[INDEX2D(1, 1)] = 1.f;
+	elements[INDEX2D(2, 2)] = 1.f;
+	elements[INDEX2D(3, 3)] = 1.f;
+}
