ce2ae1cf3c48997248911737574edda8614cbdfd
Vyom Fadia <vyom.fadia.work@gmail.com>
2018-10-20 21:56:50 +0100

    Incorporate changes into Client for testing, make it draw a triangle too

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -9,9 +9,26 @@ using namespace phx;
 
 int main(int argc, char *argv[])
 {
-	graphics::Window* window = graphics::Window::createWindow(graphics::WindowingAPI::SDL, "Phoenix!", 1280, 720);
+	graphics::Window* window = graphics::Window::createWindow(graphics::WindowingAPI::SDL, "Phoenix!", 1280, 720, {3,3}, graphics::GLProfile::CORE);
 
 	glewInit();
+
+	float vertices[] = {
+		-1.0f, -1.0f, 0.0f,
+		1.0f, -1.0f, 0.0f,
+		0.0f, 1.0f, 0.0f
+	};
+
+	unsigned int vao;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+
+	unsigned int vbo;
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), static_cast<void*>(vertices), GL_DYNAMIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 12, nullptr);
+	glEnableVertexAttribArray(0);
 
 	while (window->isRunning())
 	{
@@ -19,6 +36,10 @@ int main(int argc, char *argv[])
 
 		glClear(GL_COLOR_BUFFER_BIT);
 		glClearColor(0.3f, 0.5f, 0.7f, 1.0f);
+
+		glBindVertexArray(vao);
+		glEnableVertexAttribArray(0);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		window->swapBuffers();
 	}
