abab5f17a435d5362e843717abc95f0dc91dcd1b
apachano <pachano.austin@gmail.com>
2018-10-21 12:23:42 -0700

    add functionality to add event functions to blocks

--- engine/src/voxels/block.cpp
+++ engine/src/voxels/block.cpp
@@ -32,6 +32,27 @@ BlockType Block::getBlockType()
     return m_blockType;
 };
 
+/////////////////////////////////////////////
+// Getters and setters for event callbacks //
+/////////////////////////////////////////////
+
+void Block::setOnPlaceCallback(void* callback) { m_onPlaceCallback = callback; }
+void* Block::getOnPlaceCallback() { return m_onPlaceCallback; }
+
+void Block::setOnBreakCallback(void* callback) { m_onBreakCallback = callback; }
+void* Block::getOnBreakCallback() { return m_onBreakCallback; }
+
+void Block::setOnInteractLeftCallback(void* callback) { m_onInteractLeftCallback = callback; }
+void* Block::getOnInteractLeftCallback() { return m_onInteractLeftCallback; }
+
+void Block::setOnInteractRightCallback(void* callback) { m_onInteractRightCallback = callback; }
+void* Block::getOnInteractRightCallback() { return m_onInteractRightCallback; }
+
+
+/////////////////////////////////////////////////////////////////
+/////////////////// Block Library Code //////////////////////////
+/////////////////////////////////////////////////////////////////
+
 void BlockLibrary::registerBlock(Block* block)
 {
     m_blockLibrary.push_back(block);
