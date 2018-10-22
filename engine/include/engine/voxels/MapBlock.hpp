ee9a6a460e7ca0d9a1715b6a47df13c0f4fefa99
Vyom Fadia <vyom.fadia.work@gmail.com>
2018-10-22 13:36:17 +0100

    Add in SonosFuer's MapBlock.hpp/cpp after it got cut out due to the merge conflict

---
+++ engine/include/engine/voxels/MapBlock.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "engine/common.hpp"
+
+namespace pheonix {
+
+        namespace voxels {
+
+                class MapBlock {
+                public:
+                        MapBlock(std::string id, int rotation);
+                        ~MapBlock();
+
+                        // Getters for universal data shared between all blocks
+                        // something getTextures();
+                        std::string getID() const;
+
+                        // Getters and setters for unique block data
+                        int getRotation() const;
+                        void setRotation(int rotation);
+
+                        int getDamage() const;
+                        void setDamage(int damage);
+
+                private:
+                        std::string m_id;
+                        int m_rotation;
+                        int m_damage;
+                };
+        }
+}
