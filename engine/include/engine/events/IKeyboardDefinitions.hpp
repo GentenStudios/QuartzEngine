40cda6d1e1d1b15aca248f6189826555c5b23738
Vyom Fadia <vyom.fadia.work@gmail.com>
2018-10-24 14:09:27 +0100

    Write up basic SDL Keyboard definitions and make simple IKeyboardDefinitions as something which the main program interacts with.

---
+++ engine/include/engine/events/IKeyboardDefinitions.hpp
@@ -0,0 +1,128 @@
+#pragma once
+
+#include "engine/common.hpp"
+
+namespace phx
+{
+	namespace events
+	{
+		
+		enum IKeyboardDefinitions
+		{
+			KEY_UNKNOWN,
+
+			KEY_A,
+			KEY_B,
+			KEY_C,
+			KEY_D,
+			KEY_E,
+			KEY_F,
+			KEY_G,
+			KEY_H,
+			KEY_I,
+			KEY_J,
+			KEY_K,
+			KEY_L,
+			KEY_M,
+			KEY_N,
+			KEY_O,
+			KEY_P,
+			KEY_Q,
+			KEY_R,
+			KEY_S,
+			KEY_T,
+			KEY_U,
+			KEY_V,
+			KEY_W,
+			KEY_X,
+			KEY_Y,
+			KEY_Z,
+
+			KEY_1,
+			KEY_2,
+			KEY_3,
+			KEY_4,
+			KEY_5,
+			KEY_6,
+			KEY_7,
+			KEY_8,
+			KEY_9,
+			KEY_0,
+
+			KEY_RETURN,
+			KEY_ESCAPE,
+			KEY_BACKSPACE,
+			KEY_TAB,
+			KEY_SPACE,
+			KEY_MINUS,
+			KEY_EQUALS,
+
+			KEY_LEFTBRACKET,
+			KEY_RIGHTBRACKET,
+			KEY_BACKSLASH,
+			KEY_NONUSHASH,
+			KEY_SEMICOLON,
+			KEY_APOSTROPHE,
+			KEY_GRAVE,
+			KEY_COMMA,
+			KEY_PERIOD,
+			KEY_SLASH,
+
+			KEY_F1,
+			KEY_F2,
+			KEY_F3,
+			KEY_F4,
+			KEY_F5,
+			KEY_F6,
+			KEY_F7,
+			KEY_F8,
+			KEY_F9,
+			KEY_F10,
+			KEY_F11,
+			KEY_F12,
+			KEY_F13,
+			KEY_F14,
+			KEY_F15,
+			KEY_F16,
+			KEY_F17,
+			KEY_F18,
+			KEY_F19,
+			KEY_F20,
+			KEY_F21,
+			KEY_F22,
+			KEY_F23,
+			KEY_F24,
+			
+			KEY_UP,
+			KEY_DOWN,
+			KEY_RIGHT,
+			KEY_LEFT,
+			
+			KEY_KP_DIVIDE,
+			KEY_KP_MULTIPLY,
+			KEY_KP_MINUS,
+			KEY_KP_PLUS,
+			KEY_KP_ENTER,
+			
+			KEY_KP_1,
+			KEY_KP_2,
+			KEY_KP_3,
+			KEY_KP_4,
+			KEY_KP_5,
+			KEY_KP_6,
+			KEY_KP_7,
+			KEY_KP_8,
+			KEY_KP_9,
+			KEY_KP_0,
+			
+			KEY_LCTRL,
+			KEY_LSHIFT,
+			KEY_LALT,
+			
+			KEY_RCTRL,
+			KEY_RSHIFT,
+			KEY_RALT,
+		};
+
+	}
+}
