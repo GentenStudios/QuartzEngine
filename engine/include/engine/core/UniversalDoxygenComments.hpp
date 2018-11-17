ddbabf9f7814e887370c255b2618df2614ad1528
Vyom Fadia <vyom.fadia.work@gmail.com>
2018-11-17 15:08:48 +0000

    Start documentation spree.

---
+++ engine/include/engine/core/UniversalDoxygenComments.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+/**
+ * @brief The universal namespace for all official Phoenix code.
+ *
+ * @details ONLY Pheonix based code should be placed inside the PHX namespace.
+ *			Things like ENUMs, Classes, Functions, Structs, and similar should be placed inside the PHX Namespace,
+ *			and in any required sub-namespaces.
+ */
+namespace phx
+{
+	/**
+	 * @brief The namespace for Event related code.
+	 */
+	namespace events
+	{}
+
+	/**
+	 * @brief The Namespace for Graphical Related code.
+	 *
+	 * @details This namespace should only contain code related to the graphical part of the engine, so things like the OpenGL
+	 *			Abstraction Layer should go in here, along with something like a HUD system, etc...
+	 */
+	namespace graphics
+	{
+		/**
+		 * @brief The Namesapce for	specifically OpenGL related code.
+		 *
+		 * @details This namespace is mainly just for the OpenGL Abstraction Layer, nothing much else.
+		 *			The renderer we create later on can use this namespace, but WON'T be in it.
+		 */
+		namespace gl
+		{}
+	}
+}
