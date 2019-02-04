ea87fe43381c7c6e49ae9b42567a6bd73090520d
Barney Wilks <barneywilks1@gmail.com>
2019-02-04 00:12:08 +0000

    Removed luamod from wrong directory

--- third_party/third_party/luamod/src/function.cpp
+++
@@ -1,9 +0,0 @@
-#include <luamod/function.h>
-
-using namespace lm;
-
-int detail::LuaFunctorDispatch(lua_State* state) {
-	detail::CFunctionProxyBase* func = (detail::CFunctionProxyBase*)lua_touserdata(state, lua_upvalueindex(1));
-	int nresults = func->Call(state);
-	return nresults;
-}
