69f434861ff7f6bf827afc7b8d14f9ea5b0b609e
Vyom Fadia <vyom.fadia.work@gmail.com>
2019-02-10 19:38:09 +0000

    Write source for making a buffer depending on the rendering backend selected.

---
+++ engine/source/src/core/graphics/API/IBuffer.cpp
@@ -0,0 +1,18 @@
+#include <quartz/core/quartz-pch.hpp>
+#include <quartz/core/graphics/API/IBuffer.hpp>
+#include <quartz/core/graphics/API/Context.hpp>
+#include <quartz/core/graphics/API/gl/GLBuffer.hpp>
+
+using namespace qz::gfx::api;
+
+GraphicsResource<IBuffer> IBuffer::generateBuffer(BufferTarget target, BufferUsage usage)
+{
+	switch (Context::getRenderingAPI())
+	{
+	case RenderingAPI::OPENGL:
+		return GraphicsResource<IBuffer>(new gl::GLBuffer(target, usage));
+
+	default:
+		return nullptr;
+	}
+}
