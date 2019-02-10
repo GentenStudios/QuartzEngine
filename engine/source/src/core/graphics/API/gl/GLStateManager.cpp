1905ff40c1406b6716e7c62afe9e574d5ce4bce0
Vyom Fadia <vyom.fadia.work@gmail.com>
2019-02-10 19:42:44 +0000

    Write OpenGL VAO/state manager implementation.

---
+++ engine/source/src/core/graphics/API/gl/GLStateManager.cpp
@@ -0,0 +1,98 @@
+#include <quartz/core/quartz-pch.hpp>
+#include <quartz/core/graphics/API/gl/GLStateManager.hpp>
+
+using namespace qz::gfx::api::gl;
+using namespace qz::gfx::api;
+
+GLStateManager::GLStateManager() : m_id(0)
+{
+	glGenVertexArrays(1, &m_id);
+}
+
+GLStateManager::~GLStateManager()
+{
+	if (m_id != 0)
+		glDeleteVertexArrays(1, &m_id);
+}
+
+GLStateManager::GLStateManager(GLStateManager&& o) noexcept
+{
+	m_id = o.m_id;
+	o.m_id = 0;
+
+	m_buffers = o.m_buffers;
+}
+
+GLStateManager& GLStateManager::operator=(GLStateManager&& o) noexcept
+{
+	m_id = o.m_id;
+	o.m_id = 0;
+
+	m_buffers = std::move(o.m_buffers);
+
+	return *this;
+}
+
+void GLStateManager::bind() const
+{
+	GLCheck(glBindVertexArray(m_id));
+}
+
+void GLStateManager::unbind() const
+{
+	GLCheck(glBindVertexArray(0));
+}
+
+GraphicsResource<IBuffer> GLStateManager::retrieveBuffer(unsigned index)
+{
+	if (index < m_buffers.size())
+	{
+		return m_buffers[index];
+	}
+
+	return m_buffers[0];
+}
+
+void GLStateManager::attachBuffer(GraphicsResource<IBuffer> buffer)
+{
+	bind();
+	buffer->bind();
+	unbind();
+
+	m_buffers.emplace_back(buffer);
+}
+
+void GLStateManager::attachBufferLayout(const BufferLayout& bufferLayout, GraphicsResource<IShaderPipeline> shader)
+{
+	bind();
+
+	m_buffers[0]->bind();
+
+	for (auto& shrek : bufferLayout.getLayouts())
+	{
+		const int index = shader->retrieveAttributeLocation(shrek.name);
+
+		glVertexAttribPointer(index, 
+			shrek.elementCount, 
+			gfxToOpenGL(shrek.type), 
+			shrek.normalised ? GL_TRUE : GL_FALSE, 
+			shrek.countTillNextElement, 
+			reinterpret_cast<void*>(shrek.offset));
+
+		glEnableVertexAttribArray(index);
+		}
+
+	unbind();
+}
+
+void GLStateManager::render(unsigned int start, unsigned int count) const
+{
+	bind();
+
+	for (auto& buffer : m_buffers)
+	{
+		GLCheck(glDrawArrays(GL_TRIANGLES, start, count));
+	}
+
+	unbind();
+}
