1d778578ef8a738d5c2e39393fdb68f65497b5e9
Vyom Fadia <vyom.fadia.work@gmail.com>
2019-02-22 21:52:25 +0000

    Document the FileIO.hpp file.

--- engine/source/include/quartz/core/utilities/FileIO.hpp
+++ engine/source/include/quartz/core/utilities/FileIO.hpp
@@ -6,9 +6,17 @@ namespace qz
 {
 	namespace utils
 	{
+		/**
+		 * @brief File Input/Output wrapping class.
+		 */
 		class QZ_API FileIO
 		{
 		public:
+			/**
+			 * @brief Reads a whole file into a string.
+			 * @param filepath The path to the file needing to be read.
+			 * @return A string containing the contents of the file.
+			 */
 			static std::string readAllFile(const std::string& filepath);
 		};
 	}
