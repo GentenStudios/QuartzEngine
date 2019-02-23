77f3a29a4cde6479bd3057b001f44f15e4f7e819
Vyom Fadia <vyom.fadia.work@gmail.com>
2019-02-23 10:06:10 +0000

    Add in licenses to all files.

--- engine/source/include/quartz/core/events/Event.hpp
+++ engine/source/include/quartz/core/events/Event.hpp
@@ -1,3 +1,26 @@
+// Copyright 2019 Genten Studios
+// 
+// Redistribution and use in source and binary forms, with or without modification, are permitted provided that the 
+// following conditions are met:
+// 
+// 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the 
+// following disclaimer.
+// 
+// 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the 
+// following disclaimer in the documentation and/or other materials provided with the distribution.
+// 
+// 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote 
+// products derived from this software without specific prior written permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED 
+// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A 
+// PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY 
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR 
+// OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH 
+// DAMAGE.
+
 #pragma once
 
 #include <quartz/core/Core.hpp>
@@ -13,18 +36,18 @@ namespace qz
 		 */
 		enum class EventType
 		{
-			NONE,					//< NONE is for an event without a specific cause, it shouldn't need to be used, but it's there.
-			WINDOW_CLOSE,			//< Used for when a window is closed, it allows a developer to do certain things when a window is closed.
-			WINDOW_RESIZE,			//< Used for when a window is resized, this is mainly useful for recalculating projections/viewports for graphics.
-			WINDOW_FOCUS,			//< Used for when a window is focused or "clicked back onto". E.g. A camera can be re-enabled when the window is focused.
-			WINDOW_LOST_FOCUS,		//< Used for when a window loses focus, or is "clicked off of". E.g. A camera can be disabled when the window loses focus.
-			APP_TICK,				//< Used for when a new iteration of the main game loop runs (NOT IMPLEMENTED AS OF 19/02/2019).
-			KEY_PRESSED,			//< Used for when a key is pressed, not great for things like cameras, as the event callback will have a little "latency" to it.
-			KEY_RELEASED,			//< Used for when a key is released, not great for things like cameras, as the event callback will have a little "latency" to it.
+			NONE,			//< NONE is for an event without a specific cause, it shouldn't need to be used, but it's there.
+			WINDOW_CLOSE,		//< Used for when a window is closed, it allows a developer to do certain things when a window is closed.
+			WINDOW_RESIZE,		//< Used for when a window is resized, this is mainly useful for recalculating projections/viewports for graphics.
+			WINDOW_FOCUS,		//< Used for when a window is focused or "clicked back onto". E.g. A camera can be re-enabled when the window is focused.
+			WINDOW_LOST_FOCUS,	//< Used for when a window loses focus, or is "clicked off of". E.g. A camera can be disabled when the window loses focus.
+			APP_TICK,		//< Used for when a new iteration of the main game loop runs (NOT IMPLEMENTED AS OF 19/02/2019).
+			KEY_PRESSED,		//< Used for when a key is pressed, not great for things like cameras, as the event callback will have a little "latency" to it.
+			KEY_RELEASED,		//< Used for when a key is released, not great for things like cameras, as the event callback will have a little "latency" to it.
 			MOUSE_BUTTON_PRESSED,	//< Used for when a mouse button is pressed.
 			MOUSE_BUTTON_RELEASED,	//< Used for when a mouse button is released.
-			MOUSE_CURSOR_MOVED,		//< Used for when a mouse is moved.
-			MOUSE_SCROLLED			//< Used for when the scroll wheel on a mouse is used.
+			MOUSE_CURSOR_MOVED,	//< Used for when a mouse is moved.
+			MOUSE_SCROLLED		//< Used for when the scroll wheel on a mouse is used.
 		};
 
 		/**
@@ -32,12 +55,12 @@ namespace qz
 		 */
 		enum EventCategory : int
 		{
-			NONE = 0,					//< None is for an event with no specific category, it shouldn't need to be used, but it's there.
+			NONE = 0,				//< None is for an event with no specific category, it shouldn't need to be used, but it's there.
 			APPLICATION		= 1 << 0,	//< Application is used when the application recieves an event. For example, on the resizing of a window, or a render/tick event (both unimplemented).
 			INPUT			= 1 << 1,	//< Input is for anything related to a keyboard/mouse or any other input event that can be possible (such as joysticks and controllers in the future).
 			KEYBOARD		= 1 << 2,	//< Keyboard is for any keyboard related event, regardless of whether it is a Key Press or Key Release event.
 			MOUSE			= 1 << 3,	//< Mouse is for any mouse related event, whether it be a mouse button, scroll wheel, or just movement.
-			MOUSE_BUTTON	= 1 << 4,	//< Mouse Button is for mouse button events specifically, so only Mouse Button Releases and/or Presses.
+			MOUSE_BUTTON		= 1 << 4,	//< Mouse Button is for mouse button events specifically, so only Mouse Button Releases and/or Presses.
 		};
 
 #		define EVENT_CLASS_TYPE(type) static EventType getStaticType() { return EventType::type; } \
@@ -144,3 +167,4 @@ namespace qz
 		};
 	}
 }
+
