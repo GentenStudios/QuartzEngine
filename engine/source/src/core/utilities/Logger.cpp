060dc884eca69712698f64b08b24e1f5e1870466
Xaleth <countolaf17@gmail.com>
2019-03-03 08:50:17 -0600

    Documentation bit

--- engine/source/src/core/utilities/Logger.cpp
+++ engine/source/src/core/utilities/Logger.cpp
@@ -110,6 +110,12 @@ void Logger::initialise(const std::string& logFile, LogVerbosity verbLevel)
 	m_logFileHandle.open(Logger::m_logFile, std::ios::out | std::ios::app);
 }
 
+/**
+* @brief Destroys the logger.
+* 
+* Closes the file handle, and puts an extra newline character so the next
+* terminal prompt doesn't extend on the last logged line.
+*/
 void Logger::destroy()
 {
 	std::cout << '\n';
