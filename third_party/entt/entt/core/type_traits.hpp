ee7ddc1983358cead0f6bee00be1978a64d3611a
Alexandre Plateau <lexplt.dev@gmail.com>
2019-03-03 15:47:17 +0100

    adding EnTT

---
+++ third_party/entt/entt/core/type_traits.hpp
@@ -0,0 +1,40 @@
+#ifndef ENTT_CORE_TYPE_TRAITS_HPP
+#define ENTT_CORE_TYPE_TRAITS_HPP
+
+
+namespace entt {
+
+
+/*! @brief A class to use to push around lists of types, nothing more. */
+template<typename...>
+struct type_list {};
+
+
+/**
+ * @brief Concatenates multiple type lists into one.
+ * @tparam Type List of types.
+ * @return The given type list.
+ */
+template<typename... Type>
+constexpr auto type_list_cat(type_list<Type...> = type_list<>{}) {
+    return type_list<Type...>{};
+}
+
+
+/**
+ * @brief Concatenates multiple type lists into one.
+ * @tparam Type List of types.
+ * @tparam Other List of types.
+ * @tparam List Type list instances.
+ * @return A type list that is the concatenation of the given type lists.
+ */
+template<typename... Type, typename... Other, typename... List>
+constexpr auto type_list_cat(type_list<Type...>, type_list<Other...>, List...) {
+    return type_list_cat(type_list<Type..., Other...>{}, List{}...);
+}
+
+
+}
+
+
+#endif // ENTT_CORE_TYPE_TRAITS_HPP
