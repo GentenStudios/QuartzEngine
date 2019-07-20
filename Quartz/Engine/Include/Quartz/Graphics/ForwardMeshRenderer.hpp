359a2a216452b6bf7c006f401018b5bdc433effc
Barney Wilks <barneywilks1@gmail.com>
2019-07-20 01:42:13 +0100

    ClangFormat: Graphics Core (Headers)

--- Quartz/Engine/Include/Quartz/Graphics/ForwardMeshRenderer.hpp
+++ Quartz/Engine/Include/Quartz/Graphics/ForwardMeshRenderer.hpp
@@ -1,30 +1,35 @@
 // Copyright 2019 Genten Studios
 //
-// Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
-// following conditions are met:
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
 //
-// 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the
-// following disclaimer.
+// 1. Redistributions of source code must retain the above copyright notice,
+// this list of conditions and the following disclaimer.
 //
-// 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the
-// following disclaimer in the documentation and/or other materials provided with the distribution.
+// 2. Redistributions in binary form must reproduce the above copyright notice,
+// this list of conditions and the following disclaimer in the documentation
+// and/or other materials provided with the distribution.
 //
-// 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote
-// products derived from this software without specific prior written permission.
+// 3. Neither the name of the copyright holder nor the names of its contributors
+// may be used to endorse or promote products derived from this software without
+// specific prior written permission.
 //
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
-// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
-// PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY
-// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
-// OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
-// DAMAGE.
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
 
 #pragma once
 
-#include <Quartz/Graphics/RHI/IRenderDevice.hpp>
 #include <Quartz/Graphics/Mesh.hpp>
+#include <Quartz/Graphics/RHI/IRenderDevice.hpp>
 #include <Quartz/Math/Matrix4x4.hpp>
 #include <Quartz/Utilities/HandleAllocator.hpp>
 
@@ -34,48 +39,52 @@ namespace qz
 {
 	namespace gfx
 	{
-		struct TextureSlotHandle : utils::HandleBase { };
+		struct TextureSlotHandle : utils::HandleBase
+		{
+		};
 
 		class ForwardMeshRenderer
 		{
-		private:
-			rhi::IRenderDevice*                           m_renderDevice;
-			rhi::ShaderPipelineHandle                     m_shader;
-			rhi::UniformHandle                            m_viewMatrixUniform,
-			                                              m_projectionMatrixUniform;
+		  private:
+			rhi::IRenderDevice*       m_renderDevice;
+			rhi::ShaderPipelineHandle m_shader;
+			rhi::UniformHandle        m_viewMatrixUniform;
+			rhi::UniformHandle        m_projectionMatrixUniform;
 
-			utils::HandleAllocator<32, TextureSlotHandle> m_textureSlotsAllocator;
+			utils::HandleAllocator<32, TextureSlotHandle>
+			    m_textureSlotsAllocator;
 
 			struct MeshRenderData
 			{
-				rhi::VertexBufferHandle                   vertexBuffer;
-				Mesh*                                     mesh;
-				TextureSlotHandle                         textureSlot;
+				rhi::VertexBufferHandle vertexBuffer;
+				Mesh*                   mesh;
+				TextureSlotHandle       textureSlot;
 			};
 
-			std::vector<MeshRenderData>                   m_meshes;
+			std::vector<MeshRenderData> m_meshes;
+			Matrix4x4                   m_viewMatrix;
+			Matrix4x4                   m_projectionMatrix;
+			rhi::TextureHandle          m_defaultTexture;
+			TextureSlotHandle           m_defaultTextureSlot;
 
-			Matrix4x4                                     m_viewMatrix,
-			                                              m_projectionMatrix;
+		  public:
+			ForwardMeshRenderer (rhi::IRenderDevice* renderDevice);
 
-			rhi::TextureHandle                            m_defaultTexture;
-			TextureSlotHandle                             m_defaultTextureSlot;
+			void create ();
+			void destroy ();
+			void submitMesh (Mesh* mesh);
+			void render ();
 
-		public:
-			ForwardMeshRenderer(rhi::IRenderDevice* renderDevice);
+			void setProjectionMatrix (const Matrix4x4& projection);
+			void setViewMatrix (const Matrix4x4& view);
 
-			void        create();
-			void        destroy();
-			void        submitMesh(Mesh* mesh);
-			void        render();
+			Matrix4x4 getProjectionMatrix () const
+			{
+				return m_projectionMatrix;
+			}
 
-			void        setProjectionMatrix(const Matrix4x4& projection);
-			void        setViewMatrix(const Matrix4x4& view);
-
-			Matrix4x4   getProjectionMatrix() const { return m_projectionMatrix; }
-			Matrix4x4   getViewMatrix()        const { return m_viewMatrix; }
-
-			std::size_t countTotalNumVertices();
+			Matrix4x4   getViewMatrix () const { return m_viewMatrix; }
+			std::size_t countTotalNumVertices ();
 		};
-	}
-}
+	} // namespace gfx
+} // namespace qz
