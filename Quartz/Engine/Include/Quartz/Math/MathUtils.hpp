f5a10ac82ee4864206760a47a3db78db8559e5cf
Barney Wilks <barneywilks1@gmail.com>
2019-07-20 01:35:09 +0100

    ClangFormat: Maths (Headers)

--- Quartz/Engine/Include/Quartz/Math/MathUtils.hpp
+++ Quartz/Engine/Include/Quartz/Math/MathUtils.hpp
@@ -1,25 +1,30 @@
 // Copyright 2019 Genten Studios
-// 
-// Redistribution and use in source and binary forms, with or without modification, are permitted provided that the 
-// following conditions are met:
-// 
-// 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the 
-// following disclaimer.
-// 
-// 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the 
-// following disclaimer in the documentation and/or other materials provided with the distribution.
-// 
-// 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote 
-// products derived from this software without specific prior written permission.
-// 
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED 
-// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A 
-// PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY 
-// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
-// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
-// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR 
-// OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH 
-// DAMAGE.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice,
+// this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright notice,
+// this list of conditions and the following disclaimer in the documentation
+// and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the copyright holder nor the names of its contributors
+// may be used to endorse or promote products derived from this software without
+// specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
 
 #pragma once
 
@@ -39,7 +44,7 @@ namespace qz
 		 * @param degrees The number of degrees waiting to be converted.
 		 * @return The converted value, in radians.
 		 */
-		static float degreeToRadians(const float& degrees)
+		static float degreeToRadians (const float& degrees)
 		{
 			return degrees * PI / 180.f;
 		}
@@ -49,24 +54,25 @@ namespace qz
 		 * @param radians The number of radians waiting to be converted.
 		 * @return The converted value, in degrees.
 		 */
-		static float radianToDegrees(const float& radians)
+		static float radianToDegrees (const float& radians)
 		{
 			return radians * 180.f / PI;
 		}
 
 		/**
-		 * @brief 
+		 * @brief
 		 * @tparam T The data type that needs to be clamped.
 		 * @param n The actual value needing to be clamped.
 		 * @param lower The lowest value the result should be allowed to be.
 		 * @param upper The highest value the results should be allowed to be.
-		 * @return Either the original number if the value is between the lower and upper bounds, OR the upper/lower bounds dependant
-		 *		on the value.
+		 * @return Either the original number if the value is between the lower
+		 *         and upper bounds, OR the upper/lower bounds dependant
+		 *         on the value.
 		 */
 		template <typename T>
-		T clamp(const T& n, const T& lower, const T& upper) {
-			return std::max(lower, std::min(n, upper));
+		T clamp (const T& n, const T& lower, const T& upper)
+		{
+			return std::max (lower, std::min (n, upper));
 		}
-	};
-}
-
+	}; // namespace math
+} // namespace qz
--- Quartz/Engine/Include/Quartz/Math/Vector3.hpp
+++ Quartz/Engine/Include/Quartz/Math/Vector3.hpp
@@ -1,25 +1,30 @@
 // Copyright 2019 Genten Studios
-// 
-// Redistribution and use in source and binary forms, with or without modification, are permitted provided that the 
-// following conditions are met:
-// 
-// 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the 
-// following disclaimer.
-// 
-// 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the 
-// following disclaimer in the documentation and/or other materials provided with the distribution.
-// 
-// 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote 
-// products derived from this software without specific prior written permission.
-// 
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED 
-// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A 
-// PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY 
-// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
-// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
-// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR 
-// OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH 
-// DAMAGE.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice,
+// this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright notice,
+// this list of conditions and the following disclaimer in the documentation
+// and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the copyright holder nor the names of its contributors
+// may be used to endorse or promote products derived from this software without
+// specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
 
 #pragma once
 
@@ -30,24 +35,37 @@ namespace qz
 	namespace math
 	{
 		/**
-		 * @brief A 3 Component Vector. Simplifies maths, and management of parameters.
+		 * @brief A 3 Component Vector. Simplifies maths, and management of
+		 * parameters.
 		 */
 		struct Vector3
 		{
-			union { float x; float r; };
-			union { float y; float g; };
-			union { float z; float b; };
+			union {
+				float x;
+				float r;
+			};
+
+			union {
+				float y;
+				float g;
+			};
+
+			union {
+				float z;
+				float b;
+			};
 
 			/**
-			 * @brief Default constructs a 3 component Vector, initializes all components to 0.
+			 * @brief Default constructs a 3 component Vector, initializes all
+			 * components to 0.
 			 */
-			Vector3()												: x(0), y(0), z(0) {}
-			
+			Vector3 () : x (0), y (0), z (0) {}
+
 			/**
 			 * @brief Constructs a 3 component Vector.
 			 * @param x The value that all components should be set to.
 			 */
-			Vector3(const float x)									: x(x), y(x), z(x) {}
+			Vector3 (const float x) : x (x), y (x), z (x) {}
 
 			/**
 			 * @brief Constructs a 3 component Vector.
@@ -55,34 +73,45 @@ namespace qz
 			 * @param y The value that the Y component should be set to.
 			 * @param z The value that the Z component should be set to.
 			 */
-			Vector3(const float x, const float y, const float z)	: x(x), y(y), z(z) {}
+			Vector3 (const float x, const float y, const float z)
+			    : x (x), y (y), z (z)
+			{
+			}
 
 			/**
 			 * @brief Sets all components in the vector to a scalar value.
 			 * @param scalar The value to set all components to.
 			 */
-			void set (const float scalar) { x = scalar; y = scalar; z = scalar; };
+			void set (const float scalar)
+			{
+				x = scalar;
+				y = scalar;
+				z = scalar;
+			}
 
 			/**
 			 * @brief Floors all components in the Vector.
-			 * 
-			 * Flooring (simply put) is the process of rounding down each component to the nearest integer.
+			 *
+			 * Flooring (simply put) is the process of rounding down each
+			 * component to the nearest integer.
 			 */
-			void floor();
+			void floor ();
 
 			/**
 			 * @brief Ceils all components in the Vector.
 			 *
-			 * Ceiling (simply put) is the process of rounding up each component to the nearest integer.
+			 * Ceiling (simply put) is the process of rounding up each component
+			 * to the nearest integer.
 			 */
-			void ceil();
+			void ceil ();
 
 			/**
 			 * @brief Normalizes the vector.
-			 * 
-			 * Normalization is a mathematical process, to find out more about it, read it up on the internet.
+			 *
+			 * Normalization is a mathematical process, to find out more about
+			 * it, read it up on the internet.
 			 */
-			void normalize();
+			void normalize ();
 
 			/**
 			 * @brief Crosses two vectors.
@@ -90,16 +119,17 @@ namespace qz
 			 * @param vec2 The second vector to cross.
 			 * @return The crossed product.
 			 */
-			static Vector3	cross		(const Vector3& vec1, const Vector3& vec2);
+			static Vector3 cross (const Vector3& vec1, const Vector3& vec2);
 
 			/**
 			 * @brief Normalizes a vector.
 			 * @param vec1 The vector to normalize
 			 * @return The normalized vector.
-			 * 
-			 * Normalization is a mathematical process, to find out more about it, read it up on the internet.
+			 *
+			 * Normalization is a mathematical process, to find out more about
+			 * it, read it up on the internet.
 			 */
-			static Vector3	normalize	(const Vector3& vec1);
+			static Vector3 normalize (const Vector3& vec1);
 
 			/**
 			 * @brief Calculates the Dot Product of two vectors.
@@ -107,29 +137,105 @@ namespace qz
 			 * @param vec2 The second vector in the calculation.
 			 * @return The calculated product.
 			 */
-			static float	dotProduct	(const Vector3& vec1, const Vector3& vec2);
-			
+			static float dotProduct (const Vector3& vec1, const Vector3& vec2);
+
 			///////////////////// OPERATOR OVERLOADS /////////////////////
 
-			void	operator+=	(const Vector3& other)			{ x += other.x; y += other.y; z += other.z; };
-			void	operator-=	(const Vector3& other)			{ x -= other.x; y -= other.y; z -= other.z; };
-			void	operator+=	(const float& i)				{ x += i; y += i; z += i; }
-			void	operator-=	(const float& i)				{ x -= i; y -= i; z -= i; }
+			void operator+= (const Vector3& other)
+			{
+				x += other.x;
+				y += other.y;
+				z += other.z;
+			}
 
-			Vector3 operator+	(const Vector3& other)	const	{ return Vector3(x + other.x, y + other.y, z + other.z); }
-			Vector3 operator-	(const Vector3& other)	const	{ return Vector3(x - other.x, y - other.y, z - other.z); }
-			Vector3 operator+	(const float& i)		const	{ return Vector3(x + i, y + i, z + i); }
-			Vector3 operator-	(const float& i)		const	{ return Vector3(x - i, y - i, z - i); }
+			void operator-= (const Vector3& other)
+			{
+				x -= other.x;
+				y -= other.y;
+				z -= other.z;
+			}
 
-			void	operator*=	(const Vector3& other)			{ x *= other.x; y *= other.y;	z *= other.z; };
-			void	operator/=	(const Vector3& other)			{ x /= other.x; y /= other.y;	z /= other.z; };
-			void	operator*=	(const float& scalar)			{ x *= scalar;	y *= scalar;	z *= scalar; }
-			void	operator/=	(const float& scalar)			{ x /= scalar;	y /= scalar;	z /= scalar; }
+			void operator+= (const float& i)
+			{
+				x += i;
+				y += i;
+				z += i;
+			}
 
-			Vector3 operator*	(const Vector3& other)	const	{ return Vector3(x * other.x, y * other.y, z * other.z); }
-			Vector3 operator/	(const Vector3& other)	const	{ return Vector3(x / other.x, y / other.y, z / other.z); }
-			Vector3 operator*	(const float& scalar)	const	{ return Vector3(x * scalar, y * scalar, z * scalar); }
-			Vector3 operator/	(const float& scalar)	const	{ return Vector3(x / scalar, y / scalar, z / scalar); }
+			void operator-= (const float& i)
+			{
+				x -= i;
+				y -= i;
+				z -= i;
+			}
+
+			Vector3 operator+ (const Vector3& other) const
+			{
+				return Vector3 (x + other.x, y + other.y, z + other.z);
+			}
+
+			Vector3 operator- (const Vector3& other) const
+			{
+				return Vector3 (x - other.x, y - other.y, z - other.z);
+			}
+
+			Vector3 operator+ (const float& i) const
+			{
+				return Vector3 (x + i, y + i, z + i);
+			}
+
+			Vector3 operator- (const float& i) const
+			{
+				return Vector3 (x - i, y - i, z - i);
+			}
+
+			void operator*= (const Vector3& other)
+			{
+				x *= other.x;
+				y *= other.y;
+				z *= other.z;
+			}
+
+			void operator/= (const Vector3& other)
+			{
+				x /= other.x;
+				y /= other.y;
+				z /= other.z;
+			}
+
+			void operator*= (const float& scalar)
+			{
+				x *= scalar;
+				y *= scalar;
+				z *= scalar;
+			}
+
+			void operator/= (const float& scalar)
+			{
+				x /= scalar;
+				y /= scalar;
+				z /= scalar;
+			}
+
+			Vector3 operator* (const Vector3& other) const
+			{
+				return Vector3 (x * other.x, y * other.y, z * other.z);
+			}
+
+			Vector3 operator/ (const Vector3& other) const
+			{
+				return Vector3 (x / other.x, y / other.y, z / other.z);
+			}
+
+			Vector3 operator* (const float& scalar) const
+			{
+				return Vector3 (x * scalar, y * scalar, z * scalar);
+			}
+
+			Vector3 operator/ (const float& scalar) const
+			{
+				return Vector3 (x / scalar, y / scalar, z / scalar);
+			}
 
 			///////////////////// END OPERATOR OVERLOADS /////////////////////
 		};
@@ -138,21 +244,32 @@ namespace qz
 		 * @brief A templated 3 component Vector.
 		 * @tparam T The data type of the data being stored in the vector.
 		 *
-		 * This is for allowing more data to be passed more easily, or to store them tidily and simplify code usage.
+		 * This is for allowing more data to be passed more easily, or to store
+		 * them tidily and simplify code usage.
 		 */
-		template <typename T>
-		struct TemplateVector3
+		template <typename T> struct TemplateVector3
 		{
-			union { T x; T r; };
-			union {	T y; T g; };
-			union { T z; T b; };
+			union {
+				T x;
+				T r;
+			};
 
-			TemplateVector3()				: x(T()),	y(T()), z(T())	{}
-			TemplateVector3(T x)			: x(x),		y(x),	z(x)	{}
-			TemplateVector3(T x, T y, T z)	: x(x),		y(y),	z(z)	{}		
+			union {
+				T y;
+				T g;
+			};
+
+			union {
+				T z;
+				T b;
+			};
+
+			TemplateVector3 () : x (T ()), y (T ()), z (T ()) {}
+			TemplateVector3 (T x) : x (x), y (x), z (x) {}
+			TemplateVector3 (T x, T y, T z) : x (x), y (y), z (z) {}
 		};
-	}
-}
+	} // namespace math
+} // namespace qz
 
 /**
  * @brief Compares two vectors.
@@ -160,11 +277,10 @@ namespace qz
  * @param right The right hand side of the comparison
  * @return True when both vectors are equivalent, otherwise false.
  */
-inline bool operator==(const qz::math::Vector3& left, const qz::math::Vector3& right)
+inline bool operator== (const qz::math::Vector3& left,
+                        const qz::math::Vector3& right)
 {
-	return left.x == right.x &&
-		left.y == right.y &&
-		left.z == right.z;
+	return left.x == right.x && left.y == right.y && left.z == right.z;
 }
 
 /**
@@ -173,9 +289,8 @@ inline bool operator==(const qz::math::Vector3& left, const qz::math::Vector3& r
  * @param right The right hand side of the comparison
  * @return False when both vectors are equivalent, otherwise true.
  */
-inline bool operator!=(const qz::math::Vector3& left, const qz::math::Vector3& right)
+inline bool operator!= (const qz::math::Vector3& left,
+                        const qz::math::Vector3& right)
 {
-	return left.x != right.x &&
-		left.y != right.y &&
-		left.z != right.z;
+	return left.x != right.x && left.y != right.y && left.z != right.z;
 }
