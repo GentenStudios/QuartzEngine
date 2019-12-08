48075a914a816b938b6ba17495008de4dd9f56a5
Austin <pachano.austin@gmail.com>
2019-12-07 21:07:02 -0800

    clang-format

--- Phoenix/Core/Include/Core/Voxels/Blocks.hpp
+++ Phoenix/Core/Include/Core/Voxels/Blocks.hpp
@@ -63,16 +63,15 @@ namespace phoenix
 			int                          i;
 
 		public:
-
 			BlockRegistry();
 
 			/// @brief Registers a block in the registry
-			int            	   registerBlock(const std::string& uniqueName,
-			                                 const std::string& displayName);
+			int registerBlock(const std::string& uniqueName,
+			                  const std::string& displayName);
 			/// @brief Get the Display name for a block in the registry
 			const std::string& getDisplayName(int blockId);
 			/// @brief Get the ID for a block in the registry
-			int                getBlockId(const std::string& uniqueName);
+			int getBlockId(const std::string& uniqueName);
 		};
 
 		/// @breif Metadata for a block, this is only created if a block needs
